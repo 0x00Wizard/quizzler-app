e7fbc8d471179144caa87ac5f57376bb085c2bc8
wizardxxdx <wizardxd@icloud.com>
2023-06-14 02:07:33 -0700

    Done

--- data.py
+++ data.py
@@ -4,8 +4,8 @@ parameters = {
         "amount": 10,
         "type": "boolean",
         "category": 18,
-        "type": "boolean"
 }
+
 response = requests.get("https://opentdb.com/api.php", params=parameters)
 response.raise_for_status()
 data = response.json()
